c3ab1d5aeadd310b39cf1e04cf4369e235dadcf1
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-08-05 14:57:23 +0300

    adding and deleting elements of list with transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,67 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+
+      // transition('normal <=> highlighted', animate(300)),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
-    ])
+      transition('highlighted => normal', animate(800)),
+
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange'
+        }),
+        animate(1000, style({
+          'border-radius': '50px'
+        }) ),
+        animate(500)
+      ]),
+
+    ]),
+
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }))
+      ])
+    ]),
   ]
 })
 export class AppComponent {
 
   state = 'normal';
+  wildState = 'normal';
 
   list = ['Milk', 'Sugar', 'Bread'];
 
@@ -35,9 +88,10 @@ export class AppComponent {
 
     onAnimate() {
       this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+      this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
     }
 
     onShrink() {
-
+      this.wildState = 'shrunken';
     }
 }
